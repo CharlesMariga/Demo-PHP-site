6af85c644643ec25c627d6e8ae880fcd1602f433
Charles <charlesmariga37@gmail.com>
2023-01-10 12:06:39 +0300

    feat: PDO first steps

--- index.php
+++ index.php
@@ -2,4 +2,16 @@
 
 require("function.php");
 
-require("route.php");
+// require("route.php");
+
+// connet to MySQL database.
+$dsn = "mysql:host=localhost;port=3306;dbname=myapp;charset=utf8mb4";
+$pdo = new PDO($dsn, 'root');
+
+
+$statement = $pdo->prepare("select * from posts");
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+dd($posts);
