311e404c19f7eff96a453f592ad6cc80309083d7
Charles <charlesmariga37@gmail.com>
2023-01-10 12:42:28 +0300

    feat: environments and configuration

--- index.php
+++ index.php
@@ -4,9 +4,9 @@ require("function.php");
 
 require("Database.php");
 
-// connet to MySQL database and execute a query.
+$config = require("config.php");
 
-$db = new Database();
+$db = new Database($config['database']);
 $posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
 
 foreach ($posts as $post) {
