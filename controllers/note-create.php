81d1a14e7eaf206126f64b5a4a7274c173a900e5
Charles <charlesmariga37@gmail.com>
2023-01-11 12:25:11 +0300

    feat: extract a simple validator class

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,5 +1,7 @@
 <?php
 
+require "Validator.php";
+
 $heading = "Create Note";
 
 $config = require 'config.php';
@@ -9,12 +11,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $errors = [];
 
-    if (strlen($_POST['body']) < 1) {
-        $errors['body'] = 'Body is required';
-    }
-
-    if (strlen($_POST['body']) > 1000) {
-        $errors['body'] = 'The body cannot be more than 1000 characters';
+    if (!Validator::string($_POST['body'], 1, 1000)) {
+        $errors['body'] = 'A body of no more than 1,000 characters is required';
     }
 
     if (empty($errors)) {
