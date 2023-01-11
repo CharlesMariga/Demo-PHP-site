eefef61e50f1637fc9ea41bd5980823647262062
Charles <charlesmariga37@gmail.com>
2023-01-11 10:10:42 +0300

    feat: always exscape untrusted user input

--- views/note.view.php
+++ views/note.view.php
@@ -12,7 +12,7 @@ require('partials/banner.php');
         <p class="mb-6">
             <a href="/notes" class="text-blue-500 underline">&larr; Go back</a>
         </p>
-        <p><?= $note['body'] ?></p>
+        <p><?= htmlspecialchars($note['body']) ?></p>
     </div>
 </main>
 
