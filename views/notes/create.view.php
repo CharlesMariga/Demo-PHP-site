ad1c277aedd505054049891f7b6087a631f0c6d4
Charles <charlesmariga37@gmail.com>
2023-02-13 17:40:24 +0300

    feat: one request, one controller

--- views/notes/create.view.php
+++ views/notes/create.view.php
@@ -12,7 +12,7 @@ require(base_path('views/partials/banner.php'));
         <div>
             <div class="md:grid md:grid-cols-3 md:gap-6">
                 <div class="mt-5 md:col-span-2 md:mt-0">
-                    <form action="#" method="POST">
+                    <form action="/notes" method="POST">
                         <div class="shadow sm:overflow-hidden sm:rounded-md">
                             <div class="space-y-6 bg-white px-4 py-5 sm:p-6">
 
